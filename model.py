fe42ce0ef8cb2c41406af7905567632e5bc2db83
t4w <lorenz.burgard@gmail.com>
2026-04-30 17:01:42 +0200

    WIP: model training

--- model.py
+++ model.py
@@ -1,16 +1,13 @@
-import code_train
+import code_train as data
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, accuracy_score
 
 
-X_train, y_train_sk, X_test_sk, y_test_sk = code_train.load_data()
+#X_train, y_train_sk, X_test_sk, y_test_sk = code_train.load_data()
 
 print("training an SVC model")
 svm_model = SVC(kernel="rbf", C=10)
-svm_model.fit(X_train, y_train_sk)
+svm_model.fit(data.X_train_sk, data.y_train_sk)
 
 # Evaluation
 
-
-
-y_pred_svm = svm_model.predict(X_test_sk)
